f6066365b945d6d7e100a9df2a11e771ea4fe7c8
Charles Albert Fernandes <charles.a.fernandes@hotmail.com>
2023-12-26 18:21:35 -0300

    Criação da controle para retorno do consumidor

---
+++ Projeto.GFT/ServerApi/Controllers/InvestimentoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerApi.Domain.Interfaces;
+using ServerApi.Domain.Models;
+
+namespace ServerApi.Controllers
+{
+
+
+    [Route("api/investimento")]
+    [ApiController]
+    public class InvestimentoController : Controller
+    {
+        private readonly IInvestimentoService _investimentoService;
+
+        public InvestimentoController(IInvestimentoService investmentService)
+        {
+            _investimentoService = investmentService;
+        }
+
+        [HttpPost("calcular")]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        public IActionResult CalculateInvestment([FromBody] InvestimentoRequest request)
+        {
+
+            var result = _investimentoService.CalcularInvestimento(request);
+            return Ok(result);
+        }
+    }
+}
