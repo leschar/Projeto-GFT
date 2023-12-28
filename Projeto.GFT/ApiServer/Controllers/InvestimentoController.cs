b4d11c1335601b5070dc9bf478eb54ab5c57c263
Charles Albert Fernandes <charles.a.fernandes@hotmail.com>
2023-12-28 17:35:21 -0300

    inclusao api .net framework v4.8

---
+++ Projeto.GFT/ApiServer/Controllers/InvestimentoController.cs
@@ -0,0 +1,27 @@
+﻿using ApiServer.Domain.Interfaces;
+using ApiServer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace ApiServer.Controllers
+{
+    public class InvestimentoController : ApiController
+    {
+        private readonly IInvestimentoService _investimentoService;
+
+        public InvestimentoController(IInvestimentoService investimentoService)
+        {
+            _investimentoService = investimentoService;
+        }
+
+        [Microsoft.AspNetCore.Mvc.HttpPost("calcular")]
+        [ProducesResponseType(typeof(string), 200)]
+        [ProducesResponseType(typeof(string), 404)]
+        public IHttpActionResult CalcularInvestimento([Microsoft.AspNetCore.Mvc.FromBody] InvestimentoRequest request)
+        {
+
+            var result = _investimentoService.CalcularInvestimento(request);
+            return Ok(result);
+        }
+    }
+}
---
+++ Projeto.GFT/ApiServer/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using ApiServer.Domain.Interfaces;
+using ApiServer.Domain.Services;
+using ApiServer.Infra.Repositories;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using SimpleInjector.Lifestyles;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+
+namespace ApiServer
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            // Create the container as usual.
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            // Register your types, for instance using the scoped lifestyle:
+            container.Register<IInvestimentoService, InvestimentoServices>(Lifestyle.Scoped);
+
+            container.Register<ICDIRepository, CDIRepository>(Lifestyle.Scoped);
+            container.Register<ITBRepository, TBRepository>(Lifestyle.Scoped);
+            container.Register<IImpostoRepository, ImpostoRepository>(Lifestyle.Scoped);
+
+            // This is an extension method from the integration package.
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+
+            container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver =
+                new SimpleInjectorWebApiDependencyResolver(container);
+
+        }
+    }
+}
